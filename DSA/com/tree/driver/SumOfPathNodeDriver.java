94a84fdf10a6e02feb9da23c8ba3be52ca42bfa0
Shweta <shwetasandesh151@gmail.com>
2025-06-21 19:58:36 +0530

    Sourced Refactoring

--- DSA/com/tree/driver/SumOfPathNodeDriver.java
+++ DSA/com/tree/driver/SumOfPathNodeDriver.java
@@ -1,8 +1,24 @@
 package com.tree.driver;
 
+import com.tree.tre.RightReviewTree;
+import com.tree.tre.SumOfPathNodes;
+import com.tree.tre.TreeNode;
+
+import java.util.List;
+
 public class SumOfPathNodeDriver
 {
-	public static void main(String[] args) {
-	
+	public static void main(String[] args)
+	{
+		SumOfPathNodes tree = new SumOfPathNodes();
+		tree.root = new TreeNode(1);
+		tree.root.left = new TreeNode(2);
+		tree.root.right = new TreeNode(3);
+		tree.root.left.left = new TreeNode(4);
+		tree.root.left.right = new TreeNode(5);
+		tree.root.right.left = new TreeNode(6);
+		tree.root.right.right = new TreeNode(7);
+		int sum = tree.findSumPathNummbers(tree.root);
+		System.out.println(sum);
 	}
 }
